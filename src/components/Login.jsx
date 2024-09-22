74878e5ed707fd04a95c8989a9e4bd05a6304344
Tomasz Kowalski <88949668+tomek-kowalski@users.noreply.github.com>
2024-09-22 11:46:24 +0200

    first commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,60 @@
+import { useInput } from '../hooks/useInput.js';
+import Input from './Input.jsx';
+import {isEmail, isNotEmpty, hasMinLength} from '../util/validation.js'
+
+export default function Login() {
+
+const {
+  value:emailValue,
+  handleInputChange: handleEmailChange,
+  handleInputBlur: handleEmailBlur,
+  hasError:emailHasError
+} = useInput('', (value)=> isEmail(value) && isNotEmpty(value));
+
+const {
+  value:passwordValue,
+  handleInputChange: handlePasswordChange,
+  handleInputBlur: handlePasswordBlur,
+  hasError:passwordHasError
+} = useInput('', (value)=> hasMinLength(value,6));
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log(emailValue, emailValue);
+}
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <Input 
+          label="Email" 
+          id="email" 
+          type="email" 
+          name="email"
+          onBlur={handleEmailBlur}
+          onChange={handleEmailChange}
+          value={emailValue}
+          error={emailHasError && 'Please enter a correct email'} 
+          />
+
+        <Input 
+          label="Password" 
+          id="password" 
+          type="password" 
+          name="password" 
+          onBlur={handlePasswordBlur}
+          onChange={handlePasswordChange}
+          value={passwordValue}
+          error={passwordHasError && 'Please enter a correct password'} 
+          />
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
